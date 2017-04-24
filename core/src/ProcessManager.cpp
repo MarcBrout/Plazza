10535540a2d6c97e26b47f9609bf278f6b6028eb
marc brout <marc.brout@epitech.eu>
2017-04-24 19:03:07 +0200

    Order parsing done

--- core/src/ProcessManager.cpp
+++ core/src/ProcessManager.cpp
@@ -26,7 +26,7 @@ void oneThread(threadpool::ThreadPool<std::pair<std::string, plazza::Information
 
     while (!p_data.s_over)
     {
-        if (p_data.s_orderQ.pop(&l_order))
+        if (p_data.s_orderQ.tryPop(&l_order))
         {
             try
             {
